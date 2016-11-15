b07dce06b8c71c3e4251df144d840297839d30a2
Anatoliy <govier999@gmail.com>
2016-11-15 21:35:25 +0200

    Made simple calculator

--- lesson0/lesson0/Program.cs
+++ lesson0/lesson0/Program.cs
@@ -10,7 +10,20 @@ namespace lesson0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int i1, i2;
+            Console.WriteLine("Enter first number");
+            i1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            i2 = int.Parse(Console.ReadLine());
+            int sum = i1 + i2;
+            Console.WriteLine("Sum=" + sum);
+            int rizn = i1 - i2;
+            Console.WriteLine("Rizn=" + rizn);
+            int dob = i1 * i2;
+            Console.WriteLine("Dobutok=" + dob);
+            //double chas = i1*1.0f / i2;
+            double chas=(double)(i1)/i2;
+            Console.WriteLine("Chastka=" + chas);
         }
     }
 }
