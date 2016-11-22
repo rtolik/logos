3548de86d70a10653d0e76691b50f25adcee7af3
Anatoliy <govier999@gmail.com>
2016-11-22 21:56:32 +0200

    Made more lection subject

--- lesson1/lesson1/Program.cs
+++ lesson1/lesson1/Program.cs
@@ -49,6 +49,7 @@ namespace lesson1
                 Console.WriteLine("Year isn`t intercalary");
             }
             */
+            /*
             Console.WriteLine("Please enter all triangle sides:");
             int a, b, c;
             a = int.Parse(Console.ReadLine());
@@ -60,8 +61,53 @@ namespace lesson1
             }
             else
             {
-                Console.WriteLine("This triangle doesn`t exist! \a");
+                Console.WriteLine("This triangle doesn`t exist!");
             }
+              */
+           /*
+            int a,sum=0, avg;
+            for (int i=0;i<5;i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                sum += a;
+            }
+            avg = sum / 5;
+            Console.WriteLine("Avarage rezult is: {0}" , avg);
+            */
+            /*
+            int sum=0, num, kt=0;
+            double avg;
+            Console.WriteLine("Plese enter numbers to count avarage. If you want to stop press \"0\"");
+            do
+            {
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+                kt += 1;
+            } while (num != 0);
+            avg = sum / kt;
+            Console.WriteLine("Avarage rezult is: {0}", avg);
+            */
+            Console.WriteLine("Enter number of lines");
+            int k=int.Parse(Console.ReadLine());
+            int left=22,right=22;
+            for (int i=0;i<k;i++)
+            {
+                for (int y=0;y<45;y++)
+                {
+                    if (y >= left && y <= right)
+                    {
+                        Console.Write("^");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+                left -= 1;
+                right += 1;
+            }
+
         }
     }
 }
